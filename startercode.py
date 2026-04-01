4b78b331ac0daba8817db0d74b286a011e0d11c7
Lucy Pike <lucypike@Lucys-Computer-3.local>
2026-04-01 13:40:03 -0400

    top text

--- startercode.py
+++ startercode.py
@@ -1,12 +1,10 @@
 # SI 201 HW6 (APIs, JSON, and Caching)
-# Your name:
-# Your student id:
-# Your email:
+# Your name: Lucy Pike
+# Your student id: 03123721
+# Your email: lucypike@umich.edu
 # Who or what you worked with on this homework (including generative AI like ChatGPT):
 # If you worked with generative AI also add a statement for how you used it.
-# e.g.:
-# Asked ChatGPT for help debugging and understanding the JSON structure
-#
+# 
 # Did your use of GenAI on this assignment align with your goals and guidelines in your Gen AI contract? If not, why?
 #
 # --- ARGUMENTS & EXPECTED RETURN VALUES PROVIDED --- #
